1ac616042343a8b04b415b0648c91370163370cf
KonstantinTsoy <i02233@mail.ru>
2021-10-08 07:45:00 +0600

    HomeWork Lesson5 Создан класс Employee

---
+++ src/main/java/Lesson5/Employee.java
@@ -0,0 +1,10 @@
+package Lesson5;
+
+public class Employee {
+    private String name;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+}
---
+++ src/main/java/Lesson5/MainClass.java
@@ -0,0 +1,7 @@
+package Lesson5;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+    }
+}
