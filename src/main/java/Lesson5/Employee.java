3d0f9fa6620baa242ae66d6615a56cbbbd8420f6
KonstantinTsoy <i02233@mail.ru>
2021-10-11 20:55:45 +0600

    HomeWork 5

--- src/main/java/Lesson5/Employee.java
+++ src/main/java/Lesson5/Employee.java
@@ -7,4 +7,21 @@ public class Employee {
     private String phone;
     private int salary;
     private int age;
+
+    public Employee(String name, String position, String email, String phone, int salary, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void info() {
+        System.out.println("Имя: " + name + "; Возраст: " + age + "; Должность: " + position + "; Заработная плата: " + salary + "; email: " + email + "; тел.: " + phone);
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
--- src/main/java/Lesson5/MainClass.java
+++ src/main/java/Lesson5/MainClass.java
@@ -3,5 +3,16 @@ package Lesson5;
 public class MainClass {
     public static void main(String[] args) {
 
+        Employee[] employeeArray = new Employee[5];
+        employeeArray[0] = new Employee("Бил Гейтс", "Генеральный директор", "killBill@microsoft.com", "+77771235468", 650000, 45);
+        employeeArray[1] = new Employee("Джон Сноу", "Охранник", "johnSnow@winterfall.com", "+77015533164", 200000, 28);
+        employeeArray[2] = new Employee("Вин Дизель", "Водитель-механик", "theFast@theFurious.com", "+77771111111", 500000, 34);
+        employeeArray[3] = new Employee("Киану Ривз", "Оператор ЭВМ", "neo@matrix.com", "+77773332211", 800000, 41);
+        employeeArray[4] = new Employee("Джеймс Бонд", "Уборщик", "bond@kamesBond.com", "+77770000007", 1000000, 43);
+
+        for  (Employee employee : employeeArray) {
+            if(employee.getAge() > 40)
+                employee.info();
+        }
     }
 }
